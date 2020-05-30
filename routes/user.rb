92abb83eb17df3e8444ba06f5cdcd0d629faf42e
Joe Polny <joepolny@gmail.com>
2020-05-30 16:45:15 +0000

    added routes for testing POST processing and views

--- routes/user.rb
+++ routes/user.rb
@@ -1,20 +1,45 @@
 class GoodNote < Roda
   plugin :hash_routes
+  plugin :render
 
   hash_branch 'user' do |r|
+    user_id = session['user_id']
+    current_user = User[user_id]
+
     r.on 'clients' do
-      user_id = session['user_id']
       if user_id
-        if User[user_id][:is_therapist]
-          User.all.select {|u|  u[:therapist] == user_id}
+        if current_user[:is_therapist]
+          User.all.select { |u| u[:therapist] == user_id }
         else
-          "You must be a therapist to view this page"
+          'You must be a therapist to view this page'
         end
       else
-        r.redirect "/spotify/auth"
+        r.redirect '/spotify/auth'
+      end
+    end
+
+    r.on 'therapist' do
+      r.post do
+        id = r.params['id']
+
+        if User[spotify_id: id][:is_therapist]
+          r.redirect 'therapist'
+        else
+          r.redirect "invalid?id=#{id}"
+        end
+      end
+
+      r.get 'invalid' do
+        r.params['id'] + ' is not registered as a therapist'
+      end
+
+      r.get do
+        if current_user[:therapist]
+          User[current_user[:therapist]]
+        else
+          render 'registration'
+        end
       end
     end
   end
-
-  
 end
