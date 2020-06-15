8045539994d9281f0560c9ddac770b147595c1af
Julia Cotter <julia.cotter98@gmail.com>
2020-06-15 12:37:20 -0400

    add navigation bar

--- routes/session.rb
+++ routes/session.rb
@@ -1,8 +1,13 @@
 class GoodNote < Roda
   plugin :hash_routes
   plugin :render
+  plugin :static, ["/images", "/css", "/js"]
 
   hash_branch 'session' do |r|
+    r.on 'home' do
+      render 'home'
+    end
+
     r.on 'notes' do
       r.post do
         r.redirect "notes?client=#{r.params['client']}&date=#{r.params['date']}"
