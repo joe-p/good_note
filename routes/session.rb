70b18a061c86d3be6899ced4f602f15731a9ad5c
Joe Polny <joepolny@gmail.com>
2020-06-30 20:07:54 +0000

    save and load notes to/from database

--- routes/session.rb
+++ routes/session.rb
@@ -4,16 +4,44 @@ class GoodNote < Roda
 
   hash_branch 'session' do |r|
     r.on 'notes' do
+
+      therapist_id = session['user_id']
+
+      # POST
       r.post do
         puts r.params
+
+        client_id = r.params['client']
+        date = r.params['date']
+
+        if r.params['notes']
+          Note.update_or_create(
+            {
+              therapist_id: therapist_id,
+              patient_id: client_id,
+              creation_date: date
+            },
+            content: r.params['notes']
+          )
+        end
+
         r.redirect "notes?client=#{r.params['client']}&date=#{r.params['date']}"
       end
 
-      @clients = User.all
-      @current_client = r.params['client'] || ''
-      @selected_date = r.params['date'] || '2020-01-01'
+      # GET
+      r.get do
+        @clients = User.all
+        @current_client = r.params['client'] || ''
+        @selected_date = r.params['date'] || '2020-01-01'
 
-      render 'notes'
+        note = Note[{ patient_id: @current_client, therapist_id: therapist_id, creation_date: @selected_date }]
+
+        @note_content = note ? note[:content] : ''
+
+        puts @note_content
+
+        render 'notes'
+      end
     end
   end
 end
