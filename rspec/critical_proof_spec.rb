31c19ca24e89238b06b7c744273e21b0b9c79a10
joe <joe@localhost.localdomain>
2020-07-19 21:06:19 -0400

    finished spec for crtical proof

--- rspec/critical_proof_spec.rb
+++ rspec/critical_proof_spec.rb
@@ -1,5 +1,6 @@
 require 'faraday'
 require 'json'
+require 'faraday_middleware'
 
 HOST = 'http://do.jpolny.net'.freeze
 
@@ -11,6 +12,28 @@ describe 'GoodNote' do
       parsed_output = JSON.parse get_request.body
 
       expect(parsed_output).to be_an_instance_of(Hash).or be_an_instance_of(Array)
+    end
+  end
+
+  context "when sending a GET request to an webpage URL" do
+    it "renders an HTML document" do
+      get_request = Faraday.new.get(HOST + '/session/activity')
+      expect(get_request.body).to include("<!DOCTYPE html>")
+    end
+  end
+
+  context "When sending a POST request to an webpage URL" do
+    it "redirects to an HTML document" do
+      connection = Faraday.new(:url => HOST + '/session/notes') do |faraday|
+        faraday.use FaradayMiddleware::FollowRedirects
+        faraday.adapter Faraday.default_adapter
+      end
+      
+      res = connection.post do |req|
+        req.headers['Content-Type'] = 'application/json'
+      end
+
+      expect(res.body).to include("<!DOCTYPE html>")
     end
   end
 end
